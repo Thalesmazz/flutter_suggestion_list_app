143b361044e4fb2ea26a711cc40bd2280cde363d
thazzini <mazzinithales@gmail.com>
2025-08-29 02:47:10 -0300

    feat(Login): Create LoginViewModel to handle auth logic

---
+++ lib/viewmodel/login_viewmodel.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+enum LoginState { idle, loading, success, error }
+
+class LoginViewModel extends ChangeNotifier {
+  LoginState _state = LoginState.idle;
+  String _errorMessage = '';
+
+  LoginState get state => _state;
+  String get errorMessage => _errorMessage;
+
+  void _setState(LoginState newState) {
+    _state = newState;
+    notifyListeners();
+  }
+
+  Future<bool> login(String email, String password) async {
+    _setState(LoginState.loading);
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (email == 'thales@empiricus.com.br' && password == '123456') {
+      _setState(LoginState.success);
+      return true;
+    } else {
+      _errorMessage = 'Email ou senha inválidos.';
+      _setState(LoginState.error);
+      return false;
+    }
+  }
+}
